787beda8b26281f58f37f00ddee7c4ff607ba797
P. Most <PMost@PERA-Software.com>
2014-12-21 23:19:12 +0100

    Moved OutputParameter to lang package

--- src/com/pera_software/aidkit/lang/OutputParameter.java
+++ src/com/pera_software/aidkit/lang/OutputParameter.java
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with JavaAidKit. If not, see <http://www.gnu.org/licenses/>.
 
-package com.pera_software.aidkit.util.function;
+package com.pera_software.aidkit.lang;
 
 /**
  * @author P. Most
