e37ca91a679ca89032bf6c329fbf0df4d76ed44f
P. Most <PMost@PERA-Software.com>
2015-05-08 18:11:36 +0200

    Made HostNameResolver warning free for null analysis

--- src/com/pera_software/aidkit/net/HostNameResolver.java
+++ src/com/pera_software/aidkit/net/HostNameResolver.java
@@ -20,6 +20,8 @@ package com.pera_software.aidkit.net;
 import java.lang.reflect.*;
 import java.net.*;
 import java.util.*;
+import org.eclipse.jdt.annotation.*;
+import static com.pera_software.aidkit.eclipse.NullObjects.*;
 
 /*
  * According to this article http://support.microsoft.com/kb/308512 the windows
@@ -39,13 +41,13 @@ import java.util.*;
  * String hostName = InetAddress.getHostName();
  * long after = System.currentTimeMillis();
  */
-//#############################################################################
+//##################################################################################################
 
 public class HostNameResolver
 {
-	private static HashMap< InetAddress, String > s_hostNameCache = new HashMap<>();
+	private static HashMap< @Nullable InetAddress, @Nullable String > s_hostNameCache = new HashMap<>();
 
-	//===========================================================================
+	//==============================================================================================
 
 	public static synchronized String getHostName( InetAddress address )
 	{
@@ -75,6 +77,6 @@ public class HostNameResolver
 
 			hostName = address.getHostName();
 		}
-		return ( hostName );
+		return requireNonNull( hostName );
 	}
 }
---
+++ test/com/pera_software/aidkit/net/HostNameResolverTest.java
@@ -0,0 +1,38 @@
+// Copyright 2015 Peter Most, PERA Software Solutions GmbH
+//
+// This file is part of the JavaAidKit library.
+//
+// JavaAidKit is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// JavaAidKit is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with JavaAidKit. If not, see <http://www.gnu.org/licenses/>.
+
+package com.pera_software.aidkit.net;
+
+import java.net.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static com.pera_software.aidkit.eclipse.NullObjects.*;
+
+/**
+ * @author P. Most
+ *
+ */
+public class HostNameResolverTest {
+
+	@Test
+	@SuppressWarnings( "static-method" )
+	public void testGetHostName() throws Exception {
+		InetAddress address = InetAddress.getByName( "192.0.0.48" );
+		String hostName = HostNameResolver.getHostName( requireNonNull( address ));
+		assertNotNull( hostName );
+	}
+}
